b5f53f4c0d037cae28b2e9cc1761cf4be9ee93c0
LucasBanus <lucasbanus2000@gmail.com>
2021-09-23 18:19:10 -0500

    Started the fetch

--- PodcastListContainer.tsx
+++ PodcastListContainer.tsx
@@ -5,10 +5,15 @@ import PodcastList from './PodcastList';
 // View -> UIView
 // Android -> IView
 
+const URL_Back = "https://vizbuzz-backend.herokuapp.com/view-transcripts";
+const key_to_transcripts = "transcripts";
+
 export default function PodcastListContainer():React.ReactElement{
 
     // Perform the get operation
     const podcastNames = [{key: 'Podcast 1'}, {key: 'Podcast 2'}, {key: 'Podcast 3'}];
+
+    let transcripts = fetch(URL_Back).then(res => res.json()).then(jso => jso[key_to_transcripts]).catch(error => console.log(error));
     
     return (
         <View>
