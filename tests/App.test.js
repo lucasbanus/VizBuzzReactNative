9836976b9a9a52869b07c1457ca2d73bb3d9b067
Rashi Bose <rb63@rice.edu>
2021-10-01 16:05:47 -0500

    started writing a test

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,6 +1,20 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react-native";
 
 import App from "./../App";
+import { tsExternalModuleReference, exportAllDeclaration } from "@babel/types";
 
-describe("<App />", () => {});
+// describe("<App />", () => {
+//   it("has 1 child", () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     expect(tree.children.length).toBe(1);
+//   });
+// });
+
+test("test the PodcastListContainer", () => {
+  render(<PodcastListContainer />);
+  // fireEvent.press(getByText('Podcast 1'));
+
+  expect.stringContaining("Your Podcasts");
+});
