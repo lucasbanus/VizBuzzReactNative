3009880e171a1aa04ea3b059351cdfb754449e8f
Rashi Bose <rb63@rice.edu>
2021-10-04 13:38:27 -0500

    working on figuring out testing; the code is really messy right now but will be cleaned up later

--- components/podcasts/PodcastList.tsx
+++ components/podcasts/PodcastList.tsx
@@ -1,60 +1,76 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TouchableHighlight } from 'react-native';
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableHighlight
+} from "react-native";
 
-import {PodcastInfo} from '../../types/types';
+import { PodcastInfo } from "../../types/types";
 
 export type Props = {
-  podcastNames: Array<PodcastInfo>,
-  openPodcast: () => void, 
-  selectPodcast : (idx : number) => void, 
+  podcastNames: Array<PodcastInfo>;
+  openPodcast: () => void;
+  selectPodcast: (idx: number) => void;
 };
 
 const PodcastList = (props: Props) => {
-
-  const pressPodcast = (idx : number) => {
+  const pressPodcast = (idx: number) => {
     props.selectPodcast(idx);
     props.openPodcast();
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Your Podcasts</Text>
-       <FlatList
+      <Text style={styles.title} testID="title">
+        Your Podcasts
+      </Text>
+      <FlatList
         data={props.podcastNames}
-        renderItem={({item}) => 
-        <TouchableHighlight underlayColor = '#ccc' style={styles.touchable} onPress={() => pressPodcast(item.idx)}>
-          <Text style={styles.textInside}>{item.name}</Text>
-        </TouchableHighlight>}
+        testID="list"
+        renderItem={({ item }) => (
+          <TouchableHighlight
+            underlayColor="#ccc"
+            style={styles.touchable}
+            onPress={() => pressPodcast(item.idx)}
+          >
+            <Text style={styles.textInside} testID={item.name}>
+              {item.name}
+            </Text>
+          </TouchableHighlight>
+        )}
       />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%', 
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%"
   },
   item: {
     padding: 10,
     fontSize: 18,
-    height: 44,
+    height: 44
   },
   title: {
-    fontSize: 30, 
-    paddingBottom: 10,
-  }, 
+    fontSize: 30,
+    paddingBottom: 10
+  },
   textInside: {
-    fontSize: 20, 
-    color: "#0074FF",
-  }, 
-  touchable : {
-    borderColor : "#DEDEDE", 
-    borderWidth: 1, 
+    fontSize: 20,
+    color: "#0074FF"
+  },
+  touchable: {
+    borderColor: "#DEDEDE",
+    borderWidth: 1,
     padding: 10,
-    width : "100%"
+    width: "100%"
   }
 });
 
--- tests/App.test.js
+++ tests/App.test.js
@@ -1,20 +1,47 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
 
 import App from "./../App";
-import { tsExternalModuleReference, exportAllDeclaration } from "@babel/types";
+import PodcastList from "../components/podcasts/PodcastList";
+import PodcastListContainer from "../components/podcasts/PodcastListContainer";
 
-// describe("<App />", () => {
-//   it("has 1 child", () => {
-//     const tree = renderer.create(<App />).toJSON();
-//     expect(tree.children.length).toBe(1);
-//   });
-// });
+const names = ["hello1", "hello2", "hello3"];
+const open = () => true;
+const selectPodcast = () => 0;
 
-test("test the PodcastListContainer", () => {
-  render(<PodcastListContainer />);
-  // fireEvent.press(getByText('Podcast 1'));
+// export type Props = {
+//   podcastNames: Array<PodcastInfo>;
+//   openPodcast: () => void;
+//   selectPodcast: (idx: number) => void;
+// };
 
-  expect.stringContaining("Your Podcasts");
+test("test the PodcastListContainer", async () => {
+  const { getAllByText, queryByTestId, getByTestId } = render(
+    <PodcastList
+      podcastNames={names}
+      openPodcast={open}
+      selectPodcast={selectPodcast}
+    />
+    // <PodcastListContainer />
+  );
+  expect(getAllByText("Your Podcasts").length).toBe(1);
+  expect(getByTestId("list"));
+  // expect(getByTestId("hello1"));
+  // expect(key).toEqual("hello1");
+  await waitFor(() => {
+    expect(getAllByText("hello1"));
+    // expect(getByTestId("hello1"));
+  });
+
+  // expect(getByTestId('hello1'));
+  // expect(getAllByText("hello1").length).toBe(1);
+  // expect(getAllByText("hello2").length).toBe(1);
+  // expect(getAllByText("hello3").length).toBe(1);
+  // expect.stringContaining("Your Podcasts");
+  // // expect.stringContaining("blah blahs");
+  // // expect.stringContaining("Podcast 2");
+  // // expect.stringContaining("Podcast 3");
+  // fireEvent.press(getByText("Podcast 1"));
+  // expect.stringContaining("Hello1");
 });
