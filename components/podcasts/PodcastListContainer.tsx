52a6cbb0023f719b97c573f0d47ffa887fb28447
Grace Nichols <graceynichols@gmail.com>
2021-10-04 16:24:54 -0500

    Hard code podcasts for demo

--- components/podcasts/PodcastListContainer.tsx
+++ components/podcasts/PodcastListContainer.tsx
@@ -12,15 +12,18 @@ import PodcastList from "./PodcastList";
 import PodcastTranscript from "./PodcastTranscript";
 import * as rssParser from "react-native-rss-parser";
 
-import { PodcastItems, PodcastInfo } from "../../types/types";
+import { PodcastItems, PodcastInfo, PodcastInfo2 } from "../../types/types";
 
 const URL_Back = "https://vizbuzz-backend.herokuapp.com/view-transcripts";
 const URL_rss = "https://feeds.simplecast.com/c2RzTGta";
 const key_to_transcripts = "transcripts";
+// TODO For rigged demo: retrieving json from local data
+const localData = require("./data.json");
 
 export default function PodcastListContainer(): React.ReactElement {
   const [isTranscript, setTranscript] = useState(false);
   const [podcastNames, setPodcastNames] = useState<PodcastInfo[] | []>([]);
+  const [podcastNames2, setPodcastNames2] = useState<PodcastInfo2[] | []>([]);
   const [currentPodcast, setCurrentPodcast] = useState(0);
 
   const closePodcast = () => {
@@ -33,9 +36,15 @@ export default function PodcastListContainer(): React.ReactElement {
 
   const getPodcasts = async () => {
     try {
+      // GET request to back end
       const response = await fetch(URL_Back);
       const json = await response.json();
       const items: Array<PodcastItems> = await json[key_to_transcripts];
+      console.log("Get request " + items);
+      //   let transcript_array = localData.transcript;
+      // let new_trans_array: Array<string> = transcript_array.map(
+      //   (x: { Word: any }) => x.Word
+      // );
       const formattedItems = items.map((pod, idx) => {
         return {
           key: pod.alias,
@@ -53,7 +62,13 @@ export default function PodcastListContainer(): React.ReactElement {
           .then(responseData => rssParser.parse(responseData))
           .then(rss => {
             console.log(rss.title);
-            console.log(rss.items.length);
+            console.log(rss.authors);
+            const pod_name = rss.title;
+            const pod_authors = rss.authors;
+            // TODO: For rigged demo only, rename podcasts
+            formattedItems[0].name = rss.title + ": Daniel Osborne";
+            formattedItems[1].name = rss.title + ": John Temerian";
+            formattedItems[2].name = rss.title + ": Taylor Hull";
           });
       } finally {
         console.log("Fetch completed");
--- types/types.tsx
+++ types/types.tsx
@@ -1,17 +1,27 @@
-
 // The format used to display
 export type PodcastInfo = {
-    key : string,
-    allText : string, 
-    name : string, 
-    color : string, 
-    idx : number,
-  }
+  key: string;
+  allText: string;
+  name: string;
+  color: string;
+  idx: number;
+};
+
+// The format used to display
+export type PodcastInfo2 = {
+  key: string;
+  allText: string;
+  name: string;
+  color: string;
+  idx: number;
+  authors: string;
+};
 
 // what is returned from the get request
 export type PodcastItems = {
-    name : string, 
-    color : string, 
-    all_text : string, 
-    alias : string,
-}
+  name: string;
+  color: string;
+  all_text: string;
+  alias: string;
+  authors: string;
+};
